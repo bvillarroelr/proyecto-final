678b6db905bc9e25416cc377570439b1e714fc94
MartinL <martin.lara.tsx@gmail.com>
2023-11-25 09:25:48 -0300

    feat: Asientos non-functional buttons

--- src/main/java/reserbus/gui/BusButton.java
+++ src/main/java/reserbus/gui/BusButton.java
@@ -1,8 +1,19 @@
 package reserbus.gui;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.net.URL;
 // Boton para acceder a la info del bus, quiza lo elimine
 public class BusButton extends JButton {
     public BusButton(int n){
+        setContentAreaFilled(false); // Hace que el fondo no sea pintado, para poder ver la imagen
+
+            URL imageURL = getClass().getResource("/AsientoNormal.jpg");
+            if (imageURL != null) {
+                ImageIcon icon = new ImageIcon(imageURL);
+                setIcon(icon);
+            } else {
+                System.err.println("No se pudo cargar la imagen.");
+            }
     }
 }
--- src/main/java/reserbus/gui/BusDisplay.java
+++ src/main/java/reserbus/gui/BusDisplay.java
@@ -5,11 +5,16 @@ import java.awt.*;
 
 public class BusDisplay extends JPanel {
     public BusDisplay() {
-        this.setLayout(new GridLayout(5,5));
+        this.setLayout(new GridLayout(5,5,25,50));
         for(int i=0;i<5;i++){
             for(int j=0;j<5;j++){
-                JLabel w = new JLabel(""+ i + "," + j);
-                this.add(w);
+                if(j!=2) {
+                    BusButton w = new BusButton(j+i/10);
+                    this.add(w);
+                }
+                else{
+                    this.add(new JLabel());
+                }
             }
         }
     }
