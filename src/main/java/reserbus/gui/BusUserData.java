ddce6711607104a86769099ca5bd135f52054454
MartinL <martin.lara.tsx@gmail.com>
2023-11-29 15:04:10 -0300

    feat: Menu now changes to BusGUI

--- src/main/java/reserbus/gui/BusUserData.java
+++ src/main/java/reserbus/gui/BusUserData.java
@@ -4,8 +4,10 @@ import java.awt.*;
 
 public class BusUserData extends JPanel{
     JButton reservar = new JButton("Reservar");
+    JButton volver = new JButton("Volver");
     public BusUserData(){
         this.setLayout(new BorderLayout());
         this.add(reservar,BorderLayout.SOUTH);
+        this.add(volver,BorderLayout.NORTH);
     }
 }
